78b1f6d63b706eff41522dc3b1994bc1a1ff1120
Bai, Ming <mbbill@gmail.com>
2026-04-07 18:02:30 -0700

    delete

--- sf-nano-core/src/spectest/mod.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(feature = "spectest")]
-pub mod wast_test_runner;
-#[cfg(feature = "spectest")]
-pub mod discovery;
-#[cfg(feature = "spectest")]
-pub mod types;
-#[cfg(feature = "spectest")]
-pub mod summary;
