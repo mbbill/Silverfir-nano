524024e82bf75f61e011d86df7e851e0b6c07024
Bai, Ming <mbbill@gmail.com>
2026-04-11 21:12:53 -0700

    fixed linux build

--- sf-nano-core/src/vm/runtime/os/signal/x64_linux.rs
+++ sf-nano-core/src/vm/runtime/os/signal/x64_linux.rs
@@ -7,15 +7,20 @@ const SIGBUS: i32 = 7;
 const SA_SIGINFO: i32 = 4;
 
 #[repr(C)]
-struct kernel_sigaction {
+struct SigSet {
+    __val: [u64; 16],
+}
+
+#[repr(C)]
+struct libc_sigaction {
     sa_sigaction: unsafe extern "C" fn(i32, *mut u8, *mut u8),
-    sa_flags: u64,
+    sa_mask: SigSet,
+    sa_flags: i32,
     sa_restorer: usize,
-    sa_mask: u64,
 }
 
 unsafe extern "C" {
-    fn sigaction(sig: i32, act: *const kernel_sigaction, oldact: *mut kernel_sigaction) -> i32;
+    fn sigaction(sig: i32, act: *const libc_sigaction, oldact: *mut libc_sigaction) -> i32;
 }
 
 // Linux x86_64 ucontext layout (glibc / musl sys/ucontext.h):
@@ -62,14 +67,30 @@ unsafe extern "C" fn signal_handler(_sig: i32, _info: *mut u8, ucontext: *mut u8
 }
 
 pub(in crate::vm::runtime) unsafe fn install_platform_handler() {
-    let act = kernel_sigaction {
+    let act = libc_sigaction {
         sa_sigaction: signal_handler,
-        sa_flags: SA_SIGINFO as u64,
+        sa_mask: SigSet { __val: [0; 16] },
+        sa_flags: SA_SIGINFO,
         sa_restorer: 0,
-        sa_mask: 0,
     };
     unsafe {
         sigaction(SIGSEGV, &act, core::ptr::null_mut());
         sigaction(SIGBUS, &act, core::ptr::null_mut());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{libc_sigaction, SigSet};
+    use core::mem::{offset_of, size_of};
+
+    #[test]
+    fn glibc_x64_sigaction_layout_matches_expected_abi() {
+        assert_eq!(size_of::<SigSet>(), 128);
+        assert_eq!(offset_of!(libc_sigaction, sa_sigaction), 0);
+        assert_eq!(offset_of!(libc_sigaction, sa_mask), 8);
+        assert_eq!(offset_of!(libc_sigaction, sa_flags), 136);
+        assert_eq!(offset_of!(libc_sigaction, sa_restorer), 144);
+        assert_eq!(size_of::<libc_sigaction>(), 152);
     }
 }
